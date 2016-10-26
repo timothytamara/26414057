9d173aff63ad89cc6a769c219da1ff689a7ed548
Timothy Tamara <Timothy Tamara>
2016-10-26 22:24:05 +0700

    Commit 26 Oktober 2016

---
+++ history4_1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+nama=raw_input("Siapa nama anda?\n")
+print "____________"
+print "Hello" + nama +"!"
---
+++ history4_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+p=input('Masukkan panjang:')
+l=input('Masukkan lebar:')
+luas=p*l
+print "Luas persegi panjang: %d"%luas
