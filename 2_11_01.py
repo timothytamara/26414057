88700305ac1669b30cbded98d73c12cec7f3d259
Timothy Tamara <Timothy Tamara>
2016-11-09 22:22:17 +0700

    Commit pertama

---
+++ 2_11_01.py
@@ -0,0 +1,8 @@
+print 'Hello, world!'
+
+name=raw_input('What is your name?\n')
+print 'Hi, %s.' %name
+
+friends= ['john', 'pat', 'gary']
+for i, name in enumerate (friends)
+	print "iteration {iteration} is
